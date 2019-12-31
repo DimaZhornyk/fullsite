847cfbf673e791ac75ab0f60cd2aaeb83ab43b0b
Дмитро Жорник <dmytro.zhornyk@ukma.edu.ua>
2019-12-31 18:16:04 +0200

    Added search functionality and good frontend

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,13 @@
+import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
-import requests
+from requests.compat import quote_plus
+from . import models
 
 # Create your views here.
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 
 def home(request):
     return render(request, 'my_app/base.html')
@@ -10,8 +15,39 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-hood'):
+            post_location = post.find(class_='result-hood').text
+        else:
+            post_location = ''
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image = BASE_IMAGE_URL.format(post_image)
+        else:
+            post_image = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image, post_location))
+
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
